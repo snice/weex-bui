731a6667cb13360227d5678e51787e8c4d8e2623
tonyyls <yulsh@bingosoft.net>
2017-11-24 14:13:35 +0800

    重构标题栏

--- src/components/bui-header.vue
+++ src/components/bui-header.vue
@@ -1,21 +1,25 @@
 <template>
-    <div class="bui-header-box" :style="styleNew">
-        <div :style="{ 'height': '30px' }" v-if="iosfixed"></div>
-        <div class="bui-header">
-            <div class="bui-header-main">
-                <text class="bui-header-title" @click="_centerClick($event)">{{title}}</text>
-            </div>
+    <div :style="{'background-color':backgroundColor}">
+        <div style="height: 40px;" v-if="iosfixed"></div>
+        <div class="bui-header" :style="{'height':height}">
             <div class="bui-header-left">
-                <bui-icon @click="_leftClick($event)" v-if="leftItem.icon||leftItem.icons" :name="leftItem.icon||leftItem.icons" :size="iconSize"
-                          :color="iconColor" class="pdr10"></bui-icon>
-                <text @click="_leftClick($event)" v-if="leftItem.text" class="bui-header-text">{{leftItem.text}}</text>
+                <bui-icon @click="_leftClick($event)" v-if="leftItem.icon||leftItem.icons"
+                          :name="leftItem.icon||leftItem.icons" :size="iconSize"
+                          :color="iconColor"></bui-icon>
+                <text @click="_leftClick($event)"  :style="{'color':textColor}" v-if="leftItem.text" class="bui-header-text">{{leftItem.text}}</text>
                 <slot name="left"></slot>
             </div>
+            <div class="bui-header-main">
+                <div :style="{width:maxTitleWidth}" v-if="title!==''">
+                    <text class="bui-header-title" :style="{'color':textColor}" @click="_centerClick($event)">{{title}}</text>
+                </div>
+                <slot name="center"></slot>
+            </div>
             <div class="bui-header-right">
-                <bui-icon @click="_rightClick($event)" v-if="rightItem.icon||rightItem.icons" :name="rightItem.icon||rightItem.icons" :size="iconSize"
-                          :color="iconColor" class="pdl10"></bui-icon>
-                <text @click="_rightClick($event)" v-if="rightItem.text" class="bui-header-text">{{rightItem.text}}
-                </text>
+                <bui-icon @click="_rightClick($event)" v-if="rightItem.icon||rightItem.icons"
+                          :name="rightItem.icon||rightItem.icons" :size="iconSize"
+                          :color="iconColor"></bui-icon>
+                <text @click="_rightClick($event)" :style="{'color':textColor}" v-if="rightItem.text" class="bui-header-text">{{rightItem.text}}</text>
                 <slot name="right"></slot>
             </div>
         </div>
@@ -25,75 +29,56 @@
 
 <script>
     var defaultItem = {
-        icon:'',
+        icon: '',
         icons: '',
         text: ''
     };
+    var platform = weex.config.env.platform.toLowerCase();
     module.exports = {
-        data:function () {
+        data: function () {
             return {}
         },
         props: {
-            styleEx: {},
             title: {
                 type: String,
-                default: ''
+                default: ""
             },
             leftItem: {
                 type: Object,
-                default: function () {
-                    return defaultItem
-                }
+                default: defaultItem
             },
             rightItem: {
                 type: Object,
-                default: function () {
-                    return defaultItem
-                }
+                default: defaultItem
             },
-            ios: {
-                type: Boolean,
-                default: true
-            },
-            iconColor:{
-                type: String,
+            textColor:{
+              type:String,
                 default:"#ffffff"
             },
-            iconSize:{
+            iconColor: {
                 type: String,
-                default:"45px"
-            }
-        },
-        data: function () {
-            return {
-                height: '117px',
-                styleNew: {},
-                style: {'background-color': '#4ca4fe'}
+                default: "#ffffff"
+            },
+            iconSize: {
+                type: String,
+                default: "48px"
+            },
+            backgroundColor: {
+                type:String,
+                default: "#4ca4fe"
+            },
+            height: {
+                type:String,
+                default: "90px"
+            },
+            maxTitleWidth:{
+                type:String,
+                default:"480px"
             }
         },
         computed: {
             iosfixed: function () {
-                var ios = false;
-                if (!this.ios) {
-                    ios = false;
-                }else{
-                    //头部位置适配
-                    var env = weex.config.env.platform.toLowerCase();
-                    switch (env) {
-                        case "ios":
-                            ios = true;
-                            break;
-                        case "android":
-                            ios = false;
-                            break;
-                        case "web":
-                            ios = false;
-                            break;
-                        default:
-                            ios = false;
-                    }
-                }
-                return ios;
+                return platform === "ios";
             }
         },
         methods: {
@@ -106,13 +91,6 @@
             _centerClick: function ($event) {
                 this.$emit('centerClick', $event);
             }
-        },
-        created: function () {
-            var env = weex.config.env.platform.toLowerCase();
-            if (!this.ios || env == "android") {
-                this.height = '100px';
-            }
-            this.styleNew = Object.assign({}, this.style, this.styleEx, {'height': this.height});
         }
     }
 </script>
