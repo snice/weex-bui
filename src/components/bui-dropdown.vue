e8b152b02fe1389c520bc3636ef972dfbcbde32e
tonyyls <yulsh@bingosoft.net>
2017-11-29 15:16:45 +0800

    重构bui-dropdown

--- src/components/bui-dropdown.vue
+++ src/components/bui-dropdown.vue
@@ -1,78 +1,152 @@
 <template>
-    <div class="bui-dropdown">
-        <bui-mask v-if="show" @click="layoutClick"></bui-mask>
-        <div v-if="show" class="bui-dropdown-box" ref="dropdownBox" :style="{'left': left, 'top': top, 'width': widthDrop}">
+    <div class="bui-dropdown" :value="value" v-if="visible">
+        <bui-mask @click="_maskClick"></bui-mask>
+        <div class="bui-dropdown-box" ref="dropdownBox" :style="{'left': left, 'top': top, 'width': width}">
             <div class="bui-dropdown-content" :style="{ 'background-color': bgColor }">
-                <slot>
-                </slot>
+                <slot></slot>
             </div>
-            <text class="bui-dropdown-jiantou" :style="{color: iconColor, fontSize: '40px', 'font-family': 'jiantou' ,'left': sanjiaoLeft}">&#xe646;</text>
-            <!--<bui-icon name="icon-jiantou" :color="iconColor" class="bui-dropdown-jiantou" :style="{'left': sanjiaoLeft}"></bui-icon>-->
+            <text class="bui-dropdown-arrow" :style="{'color':bgColor,'font-family':'jiantou','left':arrowLeft}">&#xe646;</text>
         </div>
     </div>
 </template>
-
+<style lang="sass" src="../css/dropdown.scss"></style>
 <script>
     var animation = weex.requireModule('animation');
+    var platform = weex.config.env.platform;
+    var colorWhite = "#ffffff";
     module.exports = {
-        data: function () {
+        data: () => {
             return {
-                widthDrop: "260px",
+                width: "260px",
                 left: "0px",
                 top: "0px",
-                sanjiaoLeft: "40px",
+                arrowLeft: "40px",
                 position: {
                     width: '0px',
                     height: '0px',
                     left: '0px',
                     right: '0px'
-                }
+                },
+                visible: false
             }
         },
         props: {
-            datadrop: {},
-            bgColor: {
-                type: String,
-                default: "#ffffff"
-            },
-            iconColor: {
-                type: String,
-                default: "#ffffff"
-            },
-            show: {
+            value: {
                 type: Boolean,
                 default: false
+            },
+            bgColor: {
+                type: String,
+                default: colorWhite
             },
             center: {
                 type: Boolean,
                 default: false
             },
-            autowidth: {
+            autoWidth: {
                 type: Boolean,
                 default: true
             }
         },
-        created: function () {
-            //此url可以是指向本地字体图标文件路径 也可以直接用阿里巴巴字体图标库的的字体图标地址 ,比如'https://at.alicdn.com/t/font_3ppcziztn5wpcik9.ttf'
+        watch: {
+            value(val) {
+                this.visible = val;
+            },
+            visible(val) {
+                this.$emit('input', val);
+            }
+        },
+        mounted(){
+            if (this.value) {
+                this.visible = true;
+            }
+        },
+        created () {
             var bundleUrl = weex.config.bundleUrl;
             var url = bundleUrl.split('/').slice(0, -1).join('/');
             url += '/font/jiantou.ttf';
             var domModule = weex.requireModule("dom");
-            domModule.addRule('fontFace',{
+            domModule.addRule('fontFace', {
                 'fontFamily': 'jiantou',
-                'src': "url('"+url+"')"
+                'src': "url('" + url + "')"
             });
         },
-        computed:{
-        },
         components: {
-            'bui-mask': require('./bui-mask.vue'),
+            'bui-mask': require('./bui-mask.vue')
         },
         methods: {
-            //动画操作
-            animationFn : function (el, opacity, translate, timing, fn) {
+            show(event){
+                this._reset();
+                this.$nextTick(e => {
+                    this._open(event);
+                });
+            },
+            _reset(){
+                this.width="260px";
+                this.arrowLeft="40px";
+            },
+            _open(event) {
+                var el = this.$refs.dropdownBox;
+
+                this.position = event.position;
+                //autoWidth默认true，宽度按触发元素宽度自适应，如果控制宽度可设置为false，宽度为260px
+                if (this.autoWidth) {
+                    if (this.position.width >= 260) {
+                        this.width = this.position.width;
+                        if (this.center) {
+                            this.arrowLeft = this.position.width / 2 - 20;
+                            this.left = this.position.x;
+                        } else {
+                            if ((750 - this.position.x) < 260) {
+                                this.left = this.position.x - (260 - (750 - this.position.x)) - 20;
+                                this.arrowLeft = 260 - 80;
+                            } else {
+                                this.left = this.position.x + "px";
+                            }
+                        }
+                    } else {
+                        if (this.center) {
+                            this.arrowLeft = 260 / 2 - 20;
+                            this.left = this.position.x - (260 - this.position.width) / 2;
+                        } else {
+                            if ((750 - this.position.x) < 260) {
+                                this.left = this.position.x - (260 - (750 - this.position.x)) - 20;
+                                this.arrowLeft = 260 - 80;
+                            } else {
+                                this.left = this.position.x + "px";
+                            }
+                        }
+                    }
+
+                } else {
+                    if (this.center) {
+                        this.arrowLeft = 260 / 2 - 20;
+                        this.left = this.position.x + this.position.width / 2 - 130;
+                    } else {
+                        if (this.position.x == 0) {
+                            this.left = this.position.x + 20;
+                        }
+                        if ((750 - this.position.x) <= 260) {
+                            this.left = this.position.x - (260 - (750 - this.position.x)) - 20;
+                            this.arrowLeft = 260 - 80;
+                        }
+                    }
+
+                }
+
+                if (platform == "android") {
+                    this.top = this.position.y - 60;
+                } else if (platform == "iOS") {
+                    this.top = this.position.y - 20;
+                }
+
+                var translate = 'translate(0px, ' + parseInt(this.position.height) + 'px)';
+                this._animationFn(el, "1", translate, 'ease-in');
+
+            },
+            _animationFn (el, opacity, translate, timing, fn) {
                 animation.transition(el, {
-                    styles:{
+                    styles: {
                         opacity: opacity,
                         transform: translate,
                         transformOrigin: 'center center'
@@ -80,97 +154,19 @@
                     duration: 200, //ms
                     timingFunction: timing,
                     delay: 0 //ms
-                }, function () {
+                }, () => {
                     fn && fn();
                 })
             },
-            //打开上拉菜单
-            "open": function (data) {
-                var _this = this;
-                var el = _this.$refs.dropdownBox;
-
-                _this.position = data.position;
-                //autowidth默认true，宽度按触发元素宽度自适应，如果控制宽度可设置为false，宽度为260px
-                if(_this.autowidth){
-                    if(_this.position.width >= 260) {
-                        _this.widthDrop = _this.position.width;
-                        if(_this.center){
-                            _this.sanjiaoLeft = _this.position.width/2-20;
-                            _this.left = _this.position.x;
-                        }else{
-                            if((750 - _this.position.x) < 260){
-                                _this.left = _this.position.x - (260 - (750 - _this.position.x))-20;
-                                _this.sanjiaoLeft = 260 - 80;
-                            }else{
-                                _this.left = _this.position.x + "px";
-                            }
-                        }
-                    }else{
-                        if(_this.center){
-                            _this.sanjiaoLeft = 260/2 - 20;
-                            _this.left = _this.position.x - (260-_this.position.width)/2;
-                        }else{
-                            if((750 - _this.position.x) < 260){
-                                _this.left = _this.position.x - (260 - (750 - _this.position.x))-20;
-                                _this.sanjiaoLeft = 260 - 80;
-                            }else{
-                                _this.left = _this.position.x + "px";
-                            }
-                        }
-                    }
-
-                }else{
-                    if(_this.center){
-//                        _this.sanjiaoLeft = _this.position.x + 260/2 -40;
-                        _this.sanjiaoLeft = 260/2 - 20;
-                        _this.left = _this.position.x + _this.position.width/2 - 130;
-                    }else{
-                        if(_this.position.x == 0) {
-                            _this.left = _this.position.x+20;
-                        }
-                        if((750 - _this.position.x) <= 260){
-                            _this.left = _this.position.x - (260 - (750 - _this.position.x))-20;
-                            _this.sanjiaoLeft = 260 - 80;
-                        }
-                    }
-
-                }
-                //top位置适配
-                var env = weex.config.env.platform.toLowerCase();
-
-                switch(env){
-                    case "ios":
-                        _this.top = _this.position.y-20;
-                        break;
-                    case "android":
-                        _this.top = (_this.position.y-20);
-                        break;
-                    case "web":
-                        _this.top = _this.position.y-20;
-                        break;
-                    default:
-                        console.log(env);
-                }
-
-                var translate = 'translate(0px, '+ parseInt(_this.position.height)+'px)';
-                _this.animationFn(el, "1", translate,  'ease-in');
-
-            },
-            //点击mask遮罩层
-            "layoutClick": function () {
-                var _this = this;
+            _maskClick() {
                 var el = this.$refs.dropdownBox;
-
-//                var translate = 'translate(0px, -'+parseInt(_this.position.height)+'px)';
                 var translate = 'scale(0.9, 0.9)';
-                _this.animationFn(el, "0", translate,  'ease-out', function () {
-                    _this.show = false;
-                    _this.$emit("close");
+                this._animationFn(el, "0", translate, 'ease-out', () => {
+                    this.visible = false;
+                    this.$emit("onMaskClick");
                 });
-            },
-
+            }
         }
     }
 </script>
 
-<style lang="sass" src="../css/dropdown.scss"></style>
