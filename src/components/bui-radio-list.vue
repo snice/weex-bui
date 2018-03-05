32d05ae08c46f2a55f313a1357e4e12b412553b4
liyueqing <liyueqing@bingosoft.net>
2018-03-05 10:34:04 +0800

    radio list component
    radio list component

---
+++ src/components/bui-radio-list.vue
@@ -0,0 +1,51 @@
+<template>
+    <div :class="['flex-fluid']">
+        <div class="radio-box flex-row" :class="[v.disabled ? 'disabled':'']" v-for="v in items" @click="select(v)">
+            <text class="radio-label cb-flex-9" :style="{'font-size':fontSize}">{{v.title || v.value}}</text>
+            <div class="bui-icon-box cb-flex-1" v-if="v.value === value"><bui-icon @click="select(v)"  :size="iconSize" name="ion-android-radio-button-on" :color="selectedColor"></bui-icon></div>
+            <div class="bui-icon-box cb-flex-1" v-if="v.value !== value"><bui-icon @click="select(v)"  :size="iconSize" name="ion-android-radio-button-off" :color="unSelectedColor"></bui-icon></div>
+        </div>
+    </div>
+</template>
+<style lang="sass" src="../css/radio.scss"></style>
+
+<script>
+    module.exports = {
+        props: {
+            "value": {
+                type: String,
+            },
+            "items": {
+                type: Array,
+                default: []
+            },
+            "fontSize":{
+                type:[String,Number],
+                default:32
+            },
+            "iconSize":{
+                type:[String,Number],
+                default:48
+            },
+            "selectedColor":{
+                type: String,
+                default:"#00cc66"
+            },
+            "unSelectedColor":{
+                type: String,
+                default:"#e2e2e2"
+            }
+        },
+        data() {
+            return{
+            }
+        },
+        methods: {
+            select (v) {
+                if(v.disabled) return;
+                this.$emit("change", v.value);
+                this.$emit("input", v.value);
+            }
+        },
+    }
+</script>
